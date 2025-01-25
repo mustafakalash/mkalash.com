d40e7c11cff42265e37d756261bbef6588d49517
Mustafa Kalash <me@mkalash.com>
2025-01-25 15:07:39 -0500

    Changes nav design and makes the page responsive.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faHome, faVcard, faFolder, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+
 export class HeaderComponent {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faHome,
+      faVcard,
+      faFolder,
+      faEnvelope
+    );
+  }
 }
